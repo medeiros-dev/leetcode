dd65e310877f32ee15dd43e831ac75fa6731e306
Rafael Medeiros <rafa.dev.medeiros@gmail.com>
2024-01-08 18:45:13 -0300

    :hammer: Day 16

---
+++ Array & Hashing/49.group-anagrams.go
@@ -0,0 +1,20 @@
+type Key [26]byte
+
+func strKey(str string) (key Key) {
+	for i := range str {
+		key[str[i]-'a']++
+	}
+	return
+}
+func groupAnagrams(strs []string) [][]string {
+	groups := make(map[Key][]string)
+	for _, v := range strs {
+		key := strKey(v)
+		groups[key] = append(groups[key], v)
+	}
+	result := make([][]string, 0, len(groups))
+	for _, v := range groups {
+		result = append(result, v)
+	}
+	return result
+}
