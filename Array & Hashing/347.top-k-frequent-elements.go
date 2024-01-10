fec0189286d7d3fca81773cd365299e415dea5c9
Rafael Medeiros <rafa.dev.medeiros@gmail.com>
2024-01-10 10:12:37 -0300

    :art: Improving the algo!

--- Array & Hashing/347.top-k-frequent-elements.go
+++ Array & Hashing/347.top-k-frequent-elements.go
@@ -1,22 +1,25 @@
 func topKFrequent(nums []int, k int) []int {
-	numbers := make(map[int]int)
 
+	numsSize := len(nums)
+
+	if numsSize == 1 {
+		return []int{nums[0]}
+	}
+
+	numbers := make(map[int]int)
 	for _, number := range nums {
 		numbers[number]++
 	}
 
-	mostRepeateds := make([]int, k)
-	for i := 0; i < k; i++ {
-		mostRepeated := []int{0, 0}
-		for number, repeats := range numbers {
-			if repeats > mostRepeated[1] {
-				mostRepeated[0] = number
-				mostRepeated[1] = repeats
-			}
-		}
-		mostRepeateds[i] = mostRepeated[0]
-		numbers[mostRepeated[0]] = 0
+	mostFrequent := make([][]int, numsSize+1)
+	for number, repeats := range numbers {
+		mostFrequent[repeats] = append(mostFrequent[repeats], number)
 	}
 
-	return mostRepeateds
+	var result []int
+	for i := numsSize; k > len(result); i-- {
+		result = append(result, mostFrequent[i]...)
+	}
+
+	return result
 }
